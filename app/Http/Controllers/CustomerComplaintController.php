2706fc5b91da7eefd70283b693e3bc85b0cde456
Farai <farai@flickerleap.com>
2025-11-12 16:40:51 +0200

    added suspected quality problem with a medicine database design

---
+++ app/Http/Controllers/CustomerComplaintController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerComplaintRequest;
+use App\Http\Requests\UpdateCustomerComplaintRequest;
+use App\Models\CustomerComplaint;
+
+class CustomerComplaintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCustomerComplaintRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerComplaint $customerComplaint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerComplaint $customerComplaint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCustomerComplaintRequest $request, CustomerComplaint $customerComplaint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerComplaint $customerComplaint)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_11_12_143143_create_customer_complaints_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_complaints', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('complaint_number')->nullable();
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('name_of_organisation')->nullable();
+            $table->enum('complaint_channel',['Written','Email','Telephone','Verbal','Whatsapp','Facebook','Instagram','Twitter','USSD'])->nullable()->default('USSD');
+            $table->longText('details_of_complaint')->nullable();
+            $table->string('location')->nullable();
+            $table->longText('description_of_premises')->nullable();
+            $table->string('directions_to_premises')->nullable();
+            $table->string('person_to_be_investigated_contact')->nullable();
+            $table->string('received_by')->nullable();
+            $table->string('signature')->nullable();
+            $table->string('date_received')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_complaints');
+    }
+};
