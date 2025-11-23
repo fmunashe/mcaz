288751872a888fc1a6e1a2e9034d825408dbaa85
Farai <farai@flickerleap.com>
2025-11-23 20:18:19 +0200

    run optimize:clear after deployment

--- app/ClearSession.php
+++ app/ClearSession.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 trait ClearSession
 {
     public function clearSession($sessionId): void
     {
+        $appName = Str::lower(env('APP_NAME'));
         DB::table('cache')
-            ->where('key', 'like', "laravel-cache-ussd_{$sessionId}%")
+            ->where('key', 'like', "{$appName}-cache-ussd_{$sessionId}%")
             ->delete();
 
     }
