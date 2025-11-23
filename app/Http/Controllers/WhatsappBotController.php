c9542d7a9005fb5b1584565b1a17a98ff42edba0
Farai <farai@flickerleap.com>
2025-11-23 12:54:30 +0200

    check for array errors

--- app/Http/Controllers/WhatsappBotController.php
+++ app/Http/Controllers/WhatsappBotController.php
@@ -18,24 +18,25 @@ class WhatsappBotController extends Controller
         if (isset($data['entry'][0]['changes'][0]['value']['messages'][0])) {
             $message = $data['entry'][0]['changes'][0]['value']['messages'][0];
             $messageId = $request['entry'][0]['changes'][0]['value']['messages'][0]['id'];
-        }
-        $from = $message['from'];          // WhatsApp user number
-        $text = $message['text']['body'];  // Message content
-        $ussd = Ussd::machine()->set([
-            'phone_number' => $from,
-            'session_id' => $from,
-            'input' => trim($text),
-        ])->setInitialState(Welcome::class)
-            ->setResponse(function (string $message, string $action) use ($from, $messageId) {
-                $this->markMessageAsRead($messageId);
-                $this->sendMessage($message, $from);
-                return [
-                    'title' => 'MCAZ',
-                    'message' => $message,
-                ];
-            });
 
-        return $ussd->run();
+            $from = $message['from'];          // WhatsApp user number
+            $text = $message['text']['body'];  // Message content
+            $ussd = Ussd::machine()->set([
+                'phone_number' => $from,
+                'session_id' => $from,
+                'input' => trim($text),
+            ])->setInitialState(Welcome::class)
+                ->setResponse(function (string $message, string $action) use ($from, $messageId) {
+                    $this->markMessageAsRead($messageId);
+                    $this->sendMessage($message, $from);
+                    return [
+                        'title' => 'MCAZ',
+                        'message' => $message,
+                    ];
+                });
+
+            return $ussd->run();
+        }
     }
 
 
